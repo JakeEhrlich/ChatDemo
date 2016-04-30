4ab7c23f50b85d14d24ea17f42ebb076bcd56712
Jake Ehrlich <jake.h.ehrlich@gmail.com>
2016-04-29 21:37:43 -0500

    just some start up code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,31 @@
-#[cfg(test)]
-mod test {
-    #[test]
-    fn it_works() {
+use std::net::{TcpListener, TcpStream};
+use std::sync::mpsc::{Sender, Receiver, channel};
+use std::io::{stdin, BufReader, BufRead, Write};
+use std::thread::{spawn};
+use std::sync::{Arc, Mutex};
+use std::error::{Error};
+
+fn get_line_std() -> String {
+    let mut out : String = "".to_string();
+    let _ = stdin().read_line(&mut out);
+    return out.trim_right_matches(|c| c == '\n' || c == '\r').to_string();
+}
+
+fn main() {
+    loop {
+        let cmd = get_line_std();
+        match cmd.as_str() {
+            "host" => {
+                let binder = get_line_std();
+                println!("you want to bind to {}", binder);
+            }
+            "client" => {
+                let binder = get_line_std();
+                println!("you want to connect to {}", binder);
+            }
+            _ => {
+                println!("cmd not found")
+            }
+        }
     }
 }
